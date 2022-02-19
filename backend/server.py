e15008a79d36931c3fa68f8b3dc8ee249981895f
Michael <michael.d.nath@gmail.com>
2022-02-19 03:03:57 -0800

    fixed article endpoint to accept query string

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,17 @@
-import boto3
-from flask import Flask
-from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flask import Flask, request
+from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
 
 class Article(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("url", type=str, help="must be string")
-        args = parser.parse_args()
-        url = args["url"]
+    def get(self):
+        args = request.args
+        url = args.get("url")
+        return url
         # here the scraping function should be called with the url as input
-        
+
+
 api.add_resource(Article, "/article")
 if __name__ == '__main__':
     app.run(debug=True)
