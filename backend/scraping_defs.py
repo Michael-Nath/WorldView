bcafd1c99cc7e4f4f1aaabfc58a69211eee1943a
Shounak Ray <shounak@stanford.edu>
2022-02-18 23:32:00 -0800

    First commit, structure

---
+++ backend/scraping_defs.py
@@ -0,0 +1,15 @@
+# @Author: shounak
+# @Date:   2022-02-18T23:25:20-08:00
+# @Email:  shounak@stanford.edu
+# @Filename: scraping_defs.py
+# @Last modified by:   shounak
+# @Last modified time: 2022-02-18T23:31:36-08:00
+# @Description: Scrapes the headers and text body from all the files.
+#               Most basic, source information needed. No abstraction.
+
+_ = """
+####################################################################################################
+################################ LIBRARIES, VARIABLES, AND FUNCTIONS ###############################
+#################################################################################################"""
+
+# EOF
