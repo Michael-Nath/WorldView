4b6a77d69f3bf7c8396e981b9cc8639145ba4848
Shounak Ray <shounak@stanford.edu>
2022-02-19 16:03:37 -0800

    Minor

---
+++ backend/exploration/harvester.py
@@ -0,0 +1,10 @@
+# @Author: shounak
+# @Date:   2022-02-19T15:52:24-08:00
+# @Email:  shounak@stanford.edu
+# @Filename: harvester.py
+# @Last modified by:   shounak
+# @Last modified time: 2022-02-19T15:52:29-08:00
+
+
+
+# EOF
